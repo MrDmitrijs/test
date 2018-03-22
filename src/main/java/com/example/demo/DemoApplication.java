ac7275c428c7547a018217fae34309b108d03a0e
MrDmitrijs <dmitrijs.bessmertnijs@gmail.com>
2018-03-22 12:52:16 +0200

    Update

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,25 +2,18 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
+@Controller
 @SpringBootApplication
-@RestController
 public class DemoApplication {
 
 	@RequestMapping("/")
-	@ResponseBody
-	String home() {
-		return "PIPKA!";
+	String index() {
+		return "index";
 	}
 
-	@RequestMapping("/hello")
-	@ResponseBody
-	String hello() {
-		return "Hello Arturs!";
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
